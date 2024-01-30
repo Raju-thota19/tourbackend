bc22528e874aad484ff9e6b04266cd630b9ba778
Raju-thota19 <145017480+Raju-thota19@users.noreply.github.com>
2024-01-30 17:47:49 +0530

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,31 @@
+-- Create the first database
+CREATE DATABASE tourplanservicedb;
+ 
+-- Switch to the first database
+USE tourplanservicedb;
+ 
+-- Create tables or perform other initialization steps if needed
+ 
+-- Repeat the process for the other databases
+CREATE DATABASE placeservicedb;
+USE placeservicedb;
+ 
+-- Create tables or perform other initialization steps if needed
+ 
+CREATE DATABASE userservicedb;
+USE userservicedb;
+ 
+-- Create tables or perform other initialization steps if needed
+ 
+CREATE DATABASE feedbackservicedb;
+USE feedbackservicedb;
+ 
+-- Create tables or perform other initialization steps if needed
+ 
+CREATE DATABASE adminservicedb;
+USE adminservicedb;
+ 
+-- Create tables or perform other initialization steps if needed
+ 
+-- Switch back to the default MySQL database
+USE mysql;
